0143f10328908962eeb0a2d9a02c7cb9cc11d4a5
Sttcvoid <ka4ergaa@icloud.com>
2025-01-18 18:23:27 +0500

    25.1

---
+++ assets/scripts/notify.js
@@ -0,0 +1,32 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+if (getCookie('notify') != 'vktg') {
+    setCookie('notify', 'vktg', 365);
+    $(".notify-modal").removeClass("hidden");
+    $("body").addClass("overflow-hidden");
+}
+
+$(".close-modal").on('click', function () {
+    $(".notify-modal").addClass("hidden");
+    $("body").removeClass("overflow-hidden");
+})
